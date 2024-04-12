41908f11e5e63629addddcdc8da2031667f66d2d
Jhonny Dias <ojhonny1@hotmail.com>
2024-04-12 14:50:29 -0400

    feat: protecting routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Register } from "./pages/register";
 import { Login } from "./pages/login";
 
 import { Layout } from "./components/layout";
+import { Private } from "./routes/Private";
 
 const router = createBrowserRouter([
   {
@@ -22,11 +23,19 @@ const router = createBrowserRouter([
       },
       {
         path: "/dashboard",
-        element: <Dashboard />,
+        element: (
+          <Private>
+            <Dashboard />
+          </Private>
+        ),
       },
       {
         path: "/dashboard/new",
-        element: <New />,
+        element: (
+          <Private>
+            <New />
+          </Private>
+        ),
       },
     ],
   },
